41e105db8ad58c9bb96dd506e5b9c195b631ddf8
Benterf123 <faithbenter@gmail.com>
2022-11-17 10:44:33 -0800

    fix

--- binary_trees.h
+++ binary_trees.h
@@ -83,5 +83,13 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
+int tree_is_perfect(const binary_tree_t *tree);
+int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
+int heap_extract(heap_t **root);
+
 
 #endif /* _BINARY_TREES_H_ */
